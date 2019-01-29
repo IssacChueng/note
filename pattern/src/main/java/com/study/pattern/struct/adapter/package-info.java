7933057765d4c6221167c47af964a849bbd077a1
siweizhang <siweizhang123>
2019-01-29 13:41:22 +0800

    适配器模式

--- pattern/src/main/java/com/study/pattern/struct/adapter/package-info.java
+++ pattern/src/main/java/com/study/pattern/struct/adapter/package-info.java
@@ -1,6 +1,11 @@
 /**
  * 适配器模式
  * <br/>
- * 适配器四个角色: 目标抽象类, 适配器类, 适配者类, 客户类
+ * 适配器四个角色: 目标抽象类(target), 适配器类(adapter), 适配者类(adaptee), 客户类(client)
+ * <br/>
+ * 适配器模式分类适配和对象适配
+ * <br/>
+ * 类适配: 适配器类继承适配者类<br/>
+ * 对象适配: 适配器类关联适配者类<br/>
  */
 package com.study.pattern.struct.adapter;
